2cf4fcd344f098f946d60bf8f374ab8144cf2344
joniespMSI <jonialcala@gmail.com>
2019-10-01 15:42:59 +0100

    Comenzando nuevo proyecto para eventos

---
+++ eventos/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react'
+import Header from './components/header'
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
